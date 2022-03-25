49da939f01da4b38f5b1974896a9f5dc6b16a3f0
DanMB <daniel@setsnail.com>
2022-03-25 16:24:10 +0100

    fixed cra config

--- src/Login/Logo.tsx
+++ src/Login/Logo.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
-import logo from 'assets/logo.svg'
+import logo from '@assets/logo.svg'
 
 export default styled.img.attrs({
   src: logo,
-  alt: 'Songdriver logo'
+  alt: 'Songdriver logo',
 })`
   height: 5vh;
   margin: 36px auto;
--- src/Shared/Modal.tsx
+++ src/Shared/Modal.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { css } from 'styled-components'
-import xIcon from 'assets/x.svg'
+import xIcon from '@assets/x.svg'
 
-const Grid = styled.div<{show?:boolean}>`
+const Grid = styled.div<{ show?: boolean }>`
   background-color: rgba(0, 0, 0, 0.2);
   display: grid;
   grid-template-columns: 1fr minmax(auto, 800px) 1fr;
   grid-template-rows: 1fr min-content 1fr;
-  grid-template-areas: ". . ." ". modal ." ". . .";
+  grid-template-areas: '. . .' '. modal .' '. . .';
   position: absolute;
   top: 0;
   left: 0;
@@ -19,13 +19,15 @@ const Grid = styled.div<{show?:boolean}>`
   transition: opacity 0.2s ease-out;
   padding: 60px;
 
-  ${props => !props.show && css`
-    opacity: 0;
-    pointer-events: none;
-  `}
+  ${props =>
+    !props.show &&
+    css`
+      opacity: 0;
+      pointer-events: none;
+    `}
 `
 
-const ModalContainer = styled.div<{show?:boolean}>`
+const ModalContainer = styled.div<{ show?: boolean }>`
   grid-area: modal;
   min-height: 600px;
   background-color: white;
@@ -35,9 +37,11 @@ const ModalContainer = styled.div<{show?:boolean}>`
   top: 0;
   position: relative;
 
-  ${props => !props.show && css`
-    top: 20px;
-  `}
+  ${props =>
+    !props.show &&
+    css`
+      top: 20px;
+    `}
 `
 
 const ModalContent = styled.div`
@@ -63,16 +67,25 @@ const CloseButton = styled.button`
   cursor: pointer;
 `
 
-export default function Modal ({ onClose = () => null, show = false, children = undefined, title = undefined, hideX = false }) {
-  useEffect(function () {
-    if (show) {
-      document.body.style.overflow = 'hidden'
-    } else {
-      document.body.style.overflow = 'auto'
-    }
-  }, [show])
+export default function Modal({
+  onClose = () => null,
+  show = false,
+  children = undefined,
+  title = undefined,
+  hideX = false,
+}) {
+  useEffect(
+    function () {
+      if (show) {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = 'auto'
+      }
+    },
+    [show]
+  )
 
-  function handleStopPropagate (e) {
+  function handleStopPropagate(e) {
     e.stopPropagation()
   }
 
@@ -83,9 +96,7 @@ export default function Modal ({ onClose = () => null, show = false, children = 
           {!!title && <Title>{title}</Title>}
           {!hideX && <CloseButton onClick={onClose} />}
         </Toolbar>
-        <ModalContent>
-          {children}
-        </ModalContent>
+        <ModalContent>{children}</ModalContent>
       </ModalContainer>
     </Grid>
   )
