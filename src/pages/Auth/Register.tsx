11f20bced9e898e5bc7e6b7c622734359539037b
Gustav Gram Fryd <gustav@gusgram.dk>
2024-11-30 10:30:24 +0100

    Add register route

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -3,15 +3,21 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import Button from '@components/Button'
-import { TextField } from '@components/FormFields'
+import { HookTextField as TextField } from '@components/FormFields'
 
-import useAuthForm from '@hooks/forms/useAuthForm'
 import { useDocumentTitle } from '@hooks/useDocumentTitle'
 import useErrors from '@hooks/useErrors'
 import { getRouteApi, useLocation } from '@tanstack/react-router'
 
 type AuthSearchParams = {
   email: string
+}
+
+type RegisterForm = {
+  email: string
+  password: string
+  repeatPassword: string
+  name: string
 }
 
 export function validateSearch(
@@ -26,50 +32,25 @@ export default function Register({ routePath }: { routePath: RoutePath }) {
   const { useNavigate, useSearch } = getRouteApi(routePath)
   const search: AuthSearchParams = useSearch()
 
-  const { register, handleSubmit } = useForm()
-  const isRegister = useLocation().pathname.startsWith('/register')
-  const [{ email, name, password, repeatPassword, isInvite }, setField] =
-    useAuthForm()
+  const { register, handleSubmit } = useForm<RegisterForm>()
   const [loading, setLoading] = useState(false)
   const { pushError } = useErrors()
   const navigate = useNavigate()
-  useDocumentTitle(isRegister ? 'Register' : 'Login')
+  useDocumentTitle('Register')
 
-  useEffect(() => {
-    if (isRegister && window.location.search) {
-      const email = search.email
-
-      if (email) {
-        setField('email', email)
-        setField('isInvite', true)
-      }
+  const onSubmit = async ({ password, repeatPassword }: RegisterForm) => {
+    setLoading(true)
+    if (password !== repeatPassword) {
+      pushError('Passwords must match')
+      return
     }
-  }, [setField, isRegister])
 
-  const onSubmit = async data => {
-    console.log(data)
-
-    // e.preventDefault()
-    // setLoading(true)
-    // if (isRegister) {
-    //   if (password !== repeatPassword) {
-    //     pushError('Passwords must match')
-    //     return
-    //   }
-
-    //   // if (await createUser(email.toLowerCase(), password, name)) {
-    //   //   navigate({
-    //   //     to: '/'
-    //   //   })
-    //   // } else {
-    //   //   setLoading(false)
-    //   // }
+    // if (await createUser(email.toLowerCase(), password, name)) {
+    //   navigate({
+    //     to: '/'
+    //   })
     // } else {
-    //   if (await login(email.toLowerCase(), password)) {
-    //     router.history.push(search.redirect)
-    //   } else {
-    //     setLoading(false)
-    //   }
+    //   setLoading(false)
     // }
   }
 
@@ -78,33 +59,24 @@ export default function Register({ routePath }: { routePath: RoutePath }) {
       onSubmit={handleSubmit(onSubmit)}
       className="relative mx-auto mt-32 flex w-96 max-w-full flex-grow animate-teleportIn flex-col gap-4 p-5"
     >
+      <TextField autoFocus title="Name" fieldProps={register('name')} />
       <TextField
-        autoFocus
-        onChange={value => setField('name', value)}
-        value={name}
-        name="name"
-        title="Name"
-      />
-      <TextField
-        disabled={isInvite}
-        value={email}
-        name="email"
+        disabled={search.email !== ''}
+        defaultValue={search.email}
         title="Email"
-        onChange={value => setField('email', value)}
+        fieldProps={register('email', { required: 'Email is required' })}
       />
       <TextField
-        onChange={value => setField('password', value)}
-        value={password}
-        name="password"
         type="password"
         title="Password"
+        fieldProps={register('password', { required: 'Password is required' })}
       />
       <TextField
-        onChange={value => setField('repeatPassword', value)}
-        value={repeatPassword}
-        name="repeatPassword"
         type="password"
         title="Repeat password"
+        fieldProps={register('repeatPassword', {
+          required: 'Please repeat your password'
+        })}
       />
       <Button variant="primary" type="submit">
         {loading ? 'Please wait...' : 'Register'}
