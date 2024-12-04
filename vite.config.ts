c13706b79d01c255028085692e81086625b45614
Gustav Gram Fryd <gustav@gusgram.dk>
2024-12-04 14:27:17 +0100

    Improve bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,27 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build'
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('react')) {
+              return 'react'
+            } else if (id.includes('firebase')) {
+              return 'firebase'
+            } else if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          }
+        }
+      }
     },
-    plugins: [TanStackRouterVite(), react(), viteTsconfigPaths()]
+    plugins: [TanStackRouterVite(), react(), viteTsconfigPaths()],
+    manualChunks(id) {
+      console.log(id)
+      if (id.includes('node_modules')) {
+        return id.toString().split('node_modules/')[1].split('/')[0].toString()
+      }
+    }
   }
 })
