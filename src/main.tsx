7b9eae4832888de5f4510a9d40dad8f5412283b0
Gustav <gustav@gusgram.dk>
2025-09-29 21:04:04 +0200

    Show initial spinner

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,40 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 
 import router from '~/router'
 
+import { getAuthState } from './backend/auth'
 import ErrorPopUpProvider from './components/ErrorPopUp'
 import './index.css'
+
+function App() {
+  const [showSpinner, setShowSpinner] = useState(false)
+  const [isReady, setReady] = useState(false)
+  useEffect(() => {
+    getAuthState().then(() => setReady(true))
+  }, [])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setShowSpinner(true), 200)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
+  if (!isReady && showSpinner) {
+    return (
+      <div className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+        <div className="loading" />
+      </div>
+    )
+  }
+
+  return (
+    <ErrorPopUpProvider>
+      <RouterProvider router={router} />
+    </ErrorPopUpProvider>
+  )
+}
 
 // Render the app
 const rootElement = document.getElementById('root')!
@@ -13,9 +42,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <ErrorPopUpProvider>
-        <RouterProvider router={router} />
-      </ErrorPopUpProvider>
+      <App />
     </StrictMode>
   )
 }
